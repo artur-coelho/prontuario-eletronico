79f0c5d0cd615a8a6cb78c4fc1f883dc89e96ee2
Artur Coelho <artur.rocha.coelho@gmail.com>
2023-02-22 22:48:45 -0300

    feat: created auth layout and routes

---
+++ src/router/index.js
@@ -0,0 +1,38 @@
+// Composables
+import { createRouter, createWebHistory } from "vue-router";
+
+const routes = [
+  {
+    path: "/",
+    component: () => import("@/layouts/default/DefaultLayout.vue"),
+    children: [
+      {
+        path: "",
+        name: "Home",
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () =>
+          import(/* webpackChunkName: "home" */ "@/views/HomePage.vue"),
+      },
+    ],
+  },
+  {
+    path: "/auth",
+    component: () => import("@/layouts/auth/AuthLayout.vue"),
+    children: [
+      {
+        path: "login",
+        name: "Login",
+        component: () => import("@/views/auth/LoginPage.vue"),
+      },
+    ],
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+});
+
+export default router;
---
+++ src/views/auth/CadasterPage.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>Cadaster</h1>
+</template>
