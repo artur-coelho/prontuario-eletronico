f7a1cff27ddcba6dfb09c404b3a557d18904d9a5
Artur Coelho <artur.rocha.coelho@gmail.com>
2023-02-24 20:31:55 -0300

    feat: added routes to navigation drawer

--- src/layouts/default/NavigationDrawer.vue
+++ src/layouts/default/NavigationDrawer.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-navigation-drawer floating v-model="drawer" :rail="rail" color="blue-lighten-4" permanent @click="rail = false">
+  <v-navigation-drawer v-model="drawer" :rail="rail" color="blue-lighten-4" permanent @click="rail = false">
     <v-list-item title="Minimizar menu" nav>
       <template v-slot:prepend>
         <v-btn variant="text" icon="mdi-menu" class="mr-4" @click.stop="rail = !rail"></v-btn>
@@ -13,7 +13,7 @@
 
     <v-list density="compact" nav>
       <v-list-item v-for="item in items" :key="item.title" :prepend-icon="item.icon" :title="item.title"
-        :value="item.title"></v-list-item>
+        :value="item.title" :to="item.route"></v-list-item>
 
     </v-list>
   </v-navigation-drawer>
@@ -25,9 +25,9 @@ export default {
     return {
       drawer: true,
       items: [
-        { title: 'Home', icon: 'mdi-home-city' },
-        { title: 'Estatísticas', icon: 'mdi-chart-bar' },
-        { title: 'Pacientes', icon: 'mdi-account-multiple' },
+        { title: 'Home', icon: 'mdi-home-city', route: '/' },
+        { title: 'Estatísticas', icon: 'mdi-chart-bar', route: '/' },
+        { title: 'Pacientes', icon: 'mdi-account-multiple', route: '/patients' },
       ],
       rail: true,
     }
